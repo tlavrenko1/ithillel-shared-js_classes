4459764457f235e74a15f283be45f1c4c73565d0
tlavrenko1 <lavrenko.tatianna@gmail.com>
2024-03-20 00:37:46 +0100

    homework3 done

--- js_homework3.js
+++ js_homework3.js
@@ -3,7 +3,7 @@
 і код валюти, в яку потрібно конвертувати ('EUR', 'UAH', 'GBP'). 
 Функція повинна повертати суму у вказаній валюті, використовуючи фіксовані курси валют 
 (наприклад, 1 USD = 0.9 EUR, 1 USD = 35 UAH, 1 USD = 0.8 GBP). Якщо код валюти не підтримується,
- функція повинна повертати рядок з помилкою.
+ функція повинна повертати рядок з помилкою.*/
 
 function convertCurrency(availableMoney, currency) {
     if (isNaN(availableMoney)) {
@@ -102,3 +102,92 @@ executeGetDayOfWeekFunction(5);
 executeGetDayOfWeekFunction(6);
 executeGetDayOfWeekFunction(7);
 executeGetDayOfWeekFunction("hello");
+
+/*3.Задача: Знайти число Фібоначчі
+Завдання: Напишіть функцію fibonacci, що приймає число n як аргумент і повертає n-те число 
+з послідовності Фібоначчі. Послідовність починається з 0 (нульовий елемент) і 1 (перший елемент),
+ а кожне наступне число є сумою двох попередніх чисел.*/
+
+
+ function getFibonacci(n) {
+    if (n < 0 || isNaN(n)) {
+        console.log("Unexpected input");
+    } else {
+        let firstNumber = 0;
+        let secondNumber = 1;
+        let sumNumber;
+
+        if (n === 0 || n === 1) {
+            return n;
+        }
+
+        for (let i = 2; i <= n; i++) {
+            sumNumber = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = sumNumber;
+        }
+        return sumNumber;
+    }
+}
+
+function printFunction(n) {
+    var result = getFibonacci(n);
+    if (result !== undefined) {
+        console.log(`${n} number from getFibonacci is ${result}`);
+    }
+}
+
+printFunction("test");
+printFunction(0);
+printFunction(1);
+printFunction(2);
+printFunction(3);
+printFunction(4);
+printFunction(5);
+printFunction(6);
+printFunction(7);
+printFunction(8);
+printFunction(9);
+printFunction(10); 
+
+// 4. extra task
+const prompt = require('prompt-sync')();
+let numberOfAttempts = 3;
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log(`Random number is ${randomNumber}`);
+let userNumber;
+
+function guessNumber(randomNumber, numberOfAttempts) {
+    console.log(`left ${numberOfAttempts} attempts`);
+    if (numberOfAttempts <= 0) {
+        console.log("Пока");
+        return;
+    }
+    userNumber = reguestUserNumber();
+    console.log(`User number is ${userNumber}`);
+
+    if (userNumber === randomNumber) {
+        console.log('Вгадали!');
+    } else {
+        if (randomNumber > userNumber) {
+            console.log(`${userNumber} - занадто мале`);
+        } else {
+            console.log(`${userNumber} - занадто велике`);
+        }
+        numberOfAttempts = numberOfAttempts - 1;
+        console.log('Спробуйте ще раз!');
+        guessNumber(randomNumber, numberOfAttempts);
+    }
+}
+
+function reguestUserNumber() {
+    let userPrompt = prompt('What is your number? ');
+    userNumber = parseInt(userPrompt);
+    if (isNaN(userNumber) || userNumber > 10 || userNumber < 1) {
+        console.log(`${userPrompt} is invalid value`);
+        return reguestUserNumber();
+    }
+    return userNumber;
+}
+
+guessNumber(randomNumber, numberOfAttempts);
